12225792c7d844e65fb46d49f57ec903ac63c8dd
xiaodandingzp <2440788861@qq.com>
2022-04-06 10:08:56 +0800

    提交代码

---
+++ app/src/main/java/com/example/mytodoapplication/data/FinanceProducts.kt
@@ -0,0 +1,20 @@
+package com.example.mytodoapplication.data
+
+/**
+ * Created by zp on 2022/4/3 11:22
+ */
+class FinanceProducts {
+    var productName: String? = null
+
+    //    产品周期
+    var period: Int = 1
+
+    //    产品开始日期
+    var startTime: String? = null
+
+    //    产品最近可赎回日期
+    var ransomTimeLatest: String? = null
+
+    //  收益
+    var income: Int = 0
+}
